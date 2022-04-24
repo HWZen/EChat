95cd281b99fab70b46d0a2e0c61d5c9d03f10817
HWZen <1041453977@qq.com>
2022-04-24 18:33:41 +0800

    update test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,43 @@
+import entity.User;
+import entity.UserInter;
+import entity.sql.UserInterImpl;
 import org.testng.annotations.Test;
+import server.AuthorityManager;
+import server.CommandManager;
+import server.MessageManager;
+import server.WebsocketDriver;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 @Test
 public class LoginTest {
-    public void logintest() {
-        System.out.println("loginTest");
-        System.out.println(new Date());
+    static String cookie = "TEST_COOKIE";
+
+    public void logintest() throws SQLException {
+        UserInter userInter = new UserInterImpl();
+        User user = userInter.byId("test@qq.com");
+        assert user != null;
+
+        boolean err = CommandManager.login("test@qq.com", "123456", cookie);
+        assert err;
+
+    }
+
+    public void logouttest() throws SQLException {
+        UserInter userInter = new UserInterImpl();
+        User user = userInter.byId("test@qq.com");
+        assert user != null;
+
+        boolean hasLogin = AuthorityManager.isLogin(user);
+        if (!hasLogin) {
+            boolean err = CommandManager.login(user.getId(), user.getPassword(), cookie);
+            assert err;
+            assert AuthorityManager.isLogin(user);
+        }
+
+        boolean err = CommandManager.logout("test@qq.com", cookie);
+        assert err;
+        assert !AuthorityManager.isLogin(user);
     }
 }
